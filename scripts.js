f584b29e78d5334a273d7bba49e16d4286908624
seds987 <petrovskiy.m09113@gmail.com>
2024-08-07 17:51:00 +0300

    feature:add java code

---
+++ scripts.js
@@ -0,0 +1,20 @@
+// let str  = 'hello'; //string
+// let num = 25; //number
+// let bool = false; //boolean
+// let num2; //undefined
+// let zero = null; //null
+//sumbol 
+//object
+
+// console.log(str);
+
+// let age = prompt('enter your age:');
+// console.log(age);
+
+
+
+
+function showMobileMenu() {
+    let menu = document.getElementById('mobile-menu');
+    menu.style.display = 'block';
+}
