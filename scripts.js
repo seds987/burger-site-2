1f5b32e359e3460b178564b5b71abc0a43f350bb
seds987 <petrovskiy.m09113@gmail.com>
2024-08-19 17:52:07 +0300

    feature:add  burger price

--- scripts.js
+++ scripts.js
@@ -84,12 +84,29 @@ let cheaseMinus = document.getElementById('chease-minus');
 let saladMinus = document.getElementById('salad-minus');
 let bunMinus = document.getElementById('bun-minus');
 
+
+
+const ingredientPrices = {
+    cutlet: 2.50,    // Ціна за котлету
+    mayo: 0.50,      // Ціна за майонез
+    onion: 0.30,     // Ціна за цибулю
+    tomato: 0.70,    // Ціна за помідор
+    cucumber: 0.60,  // Ціна за огірок
+    cheese: 1.00,    // Ціна за сир
+    salad: 0.40,     // Ціна за салат
+    bun: 1.50        // Ціна за булку
+};
+
+
+// cutlet
+
 cutletPlus.onclick = addCutlet;
 cutletMinus.onclick = minusCutlet;
 
 function addCutlet() {
     cutletValue++;
     cutletCount.innerHTML = cutletValue;
+    showBurger();
 }
 
 function minusCutlet() {
@@ -99,17 +116,219 @@ function minusCutlet() {
     
     cutletValue--;
     cutletCount.innerHTML = cutletValue;
+    showBurger();
 }
-
-
+// mayo
 
 mayoPlus.onclick = addMayo;
+mayoMinus.onclick = minusMayo;
 
 function addMayo() {
     mayoValue++;
     mayoCount.innerHTML = mayoValue;
+    showBurger();
 }
 
-onionPlus.onclick = addOnion;
+function minusMayo() {
+    if(mayoValue <= 0) {
+        return;
+    }
+    
+    mayoValue--;
+    mayoCount.innerHTML = mayoValue;
+    showBurger();
+}
 
-function addOnion() {}
+// onion
+
+onionPlus.onclick = addOnion;
+onionMinus.onclick = minusOnion;
+
+function addOnion() {
+    onionValue++;
+    onionCount.innerHTML = onionValue;
+    showBurger();
+}
+
+function minusOnion() {
+    if(onionValue <= 0) {
+        return;
+    }
+    
+    onionValue--;
+    onionCount.innerHTML = onionValue;
+    showBurger();
+}
+
+// tomato
+
+
+tomatoPlus.onclick = addTomato;
+tomatoMinus.onclick = minusTomato;
+
+function addTomato() {
+    tomatoValue++;
+    tomatoCount.innerHTML = tomatoValue;
+    showBurger();
+}
+
+function minusTomato() {
+    if(tomatoValue <= 0) {
+        return;
+    }
+    
+    tomatoValue--;
+    tomatoCount.innerHTML = tomatoValue;
+    showBurger();
+}
+
+// cucumber
+
+cucumberPlus.onclick = addCucumber;
+cucumberMinus.onclick = minusCucumber;
+
+function addCucumber() {
+    cucumberValue++;
+    cucumberCount.innerHTML = cucumberValue;
+    showBurger();
+}
+
+function minusCucumber() {
+    if(cucumberValue <= 0) {
+        return;
+    }
+    
+    cucumberValue--;
+    cucumberCount.innerHTML = cucumberValue;
+    showBurger();
+}
+
+// cheease
+
+cheasePlus.onclick = addChease;
+cheaseMinus.onclick = minusChease;
+
+function addChease() {
+    cheaseValue++;
+    cheaseCount.innerHTML = cheaseValue;
+    showBurger();
+}
+
+function minusChease() {
+    if(cheaseValue <= 0) {
+        return;
+    }
+    
+    cheaseValue--;
+    cheaseCount.innerHTML = cheaseValue;
+    showBurger();
+}
+
+//salad
+
+saladPlus.onclick = addSalad;
+saladMinus.onclick = minusSalad;
+
+function addSalad() {
+    saladValue++;
+    saladCount.innerHTML = saladValue;
+    showBurger();
+    
+}
+
+function minusSalad() {
+    if(saladValue <= 0) {
+        return;
+    }
+    
+    saladValue--;
+    saladCount.innerHTML = saladValue;
+    showBurger();
+}
+
+//bun
+bunPlus.onclick = addBun;
+bunMinus.onclick = minusBun;
+
+function addBun() {
+    bunValue++;
+    bunCount.innerHTML = bunValue;
+    showBurger();
+}
+
+function minusBun() {
+    if(bunValue <= 0) {
+        return;
+    }
+    
+    bunValue--;
+    bunCount.innerHTML = bunValue;
+    showBurger();
+}
+
+
+function showBurger() {
+    console.log(
+      "Burger: +++++++++++" +
+        "Cutlet:" +
+        cutletCount.innerHTML + "\n" +
+        "Mayo:" +
+        mayoCount.innerHTML + "\n" +
+        "Onion:" +
+        onionCount.innerHTML + "\n" +
+        "Tomato:" +
+        tomatoCount.innerHTML + "\n" +
+        "Cucumber:" +
+        cucumberCount.innerHTML + "\n" +
+        "Cheese:" +
+        cheaseCount.innerHTML + "\n" +
+        "Salad:" +
+        saladCount.innerHTML + "\n" +
+        "Bun:" +
+      bunCount.innerHTML
+  );
+
+  let BurgerPrice = cutletCount.innerHTML * ingredientPrices.cutlet +
+    mayoCount.innerHTML * ingredientPrices.mayo + 
+    onionCount.innerHTML * ingredientPrices.onion + 
+    tomatoCount.innerHTML * ingredientPrices.tomato +
+    cucumberCount.innerHTML * ingredientPrices.cucumber + 
+    cheaseCount.innerHTML * ingredientPrices.cheese +
+    saladCount.innerHTML * ingredientPrices.salad + 
+    bunCount.innerHTML  * ingredientPrices.bun 
+
+    document.getElementById('burger-price').innerHTML = '$' + BurgerPrice.toFixed(2);
+  
+}
+
+showBurger();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function canculateBurgerPrice(
+   cutletCount,mayoCount,onionCount,tomatoCount,
+   cucumberCount,cheaseCount,saladCount,bunCount 
+) {
+    console.log (
+        cutletCount,mayoCount,onionCount,tomatoCount,
+        cucumberCount,cheaseCount,saladCount,bunCount 
+    )
+}
+
+canculateBurgerPrice(
+    cutletCount.innerHTML,mayoCount.innerHTML,onionCount.innerHTML,tomatoCount.innerHTML,
+    cucumberCount.innerHTML,cheaseCount.innerHTML,saladCount.innerHTML,bunCount.innerHTML 
+)
